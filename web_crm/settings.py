b2b4a312c743856451f1214965f63ee3315de8fd
strong0429 <strong.lee0429@gmail.com>
2019-10-18 23:13:01 +0800

    增加login app，处理用户登录

--- web_crm/settings.py
+++ web_crm/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'teaching_app',
+    'login_app',
 ]
 
 MIDDLEWARE = [
--- web_crm/urls.py
+++ web_crm/urls.py
@@ -13,17 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
+#from django.views.generic import RedirectView
 
-from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', include('login_app.urls')),
     path('admin/', admin.site.urls),
-]
-
-urlpatterns += [
-    path('teaching_app/', include('teaching_app.urls')),
-    # path('', RedirectView.as_view(url='/teaching_app/', permanent=True)),
 ]
