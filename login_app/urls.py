3f022c955b72b3b90b6e37937520fca4639e258c
strong0429 <strong.lee0429@gmail.com>
2019-10-19 15:22:06 +0800

    优化 login_app 登录页面

--- login_app/urls.py
+++ login_app/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('index/', views.index),
     path('login/', views.login),
     path('register/', views.register),
     path('logout/', views.logout),
--- login_app/views.py
+++ login_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 
+from .models import User
+from . import forms
+
 # Create your views here.
 
 def index(request):
@@ -8,8 +11,27 @@ def index(request):
     return render(request, 'login/index.html')
 
 def login(request):
-    pass
-    return render(request, 'login/login.html')
+    if request.method == 'POST':
+        err_msg = '请输入完整的信息'
+        login_form = forms.UserForm(request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data.get('username')
+            password = login_form.cleaned_data.get('password')
+            try:
+                user = User.objects.get(name=username)
+            except:
+                err_msg = '用户不存在'
+                return render(request, 'login/login.html', locals())
+            if user.password == password:
+                return redirect('/index/')
+            else:
+                err_msg = '密码错误'
+                return render(request, 'login/login.html', locals)
+        else:
+            return render(request, 'login/login.html', locals())
+
+    login_form = forms.UserForm()
+    return render(request, 'login/login.html', locals())
 
 def register(request):
     pass
