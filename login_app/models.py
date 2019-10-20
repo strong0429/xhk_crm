757c059136f0d31d02687e0241b045472bae1486
strong0429 <strong.lee0429@gmail.com>
2019-10-20 11:40:19 +0800

    更新数据库

--- login_app/models.py
+++ login_app/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
     name = models.CharField(max_length=128, unique=True)
     password = models.CharField(max_length=256)
     c_time = models.DateTimeField(auto_now_add=True)
-    has_confirmed = models.BooleanField(default=False)
+    # has_confirmed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
@@ -17,7 +17,7 @@ class User(models.Model):
         ordering = ['c_time']
         verbose_name = '用户'
         verbose_name_plural = '用户'
-
+'''
 class ConfirmString(models.Model):
     code = models.CharField(max_length=256)
     user = models.OneToOneField('User', on_delete=models.CASCADE)
@@ -32,3 +32,4 @@ class ConfirmString(models.Model):
         ordering = ['c_time']
         verbose_name = '确认码'
         verbose_name_plural = '确认码'
+'''
